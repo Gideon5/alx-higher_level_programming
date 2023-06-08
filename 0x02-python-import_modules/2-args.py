5568b54058c918dc8bdb84317be6698a481cce1b
Gideon Mba <abaaneg@gmail.com>
2023-06-08 07:09:46 -0700

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 import sys
 
-numArgs = len(sys.argv) - 1
 
-if (numArgs == 0):
+def printNums():
+    numArgs = len(sys.argv) - 1
+
+    if (numArgs == 0):
         print("{} arguments.".format(numArgs))
-else:
-    print("{} arguments:".format(numArgs))
-    for i in range(numArgs):
-        print("{}: {}".format(i+1, sys.argv[i+1]))
+    else:
+        print("{} arguments:".format(numArgs))
+        for i in range(numArgs):
+            print("{}: {}".format(i+1, sys.argv[i+1]))
+
+
+if __name__ == "__main__":
+    printNums()
