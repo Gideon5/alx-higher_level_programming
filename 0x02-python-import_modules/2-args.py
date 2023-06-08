12a0135db56187f08da1152fdf3164d8696e5343
Gideon Mba <abaaneg@gmail.com>
2023-06-08 12:07:32 -0700

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,12 +7,13 @@ def printNums():
 
     if (numArgs == 0):
         print("{} arguments.".format(numArgs))
+    elif (numArgs == 1):
+        print("{} argument:".format(numArgs))
+        print("{}: {}".format(numArgs, sys.argv[1]))
     else:
         print("{} arguments:".format(numArgs))
         for i in range(numArgs):
             print("{}: {}".format(i+1, sys.argv[i+1]))
-
-
 
 
 if __name__ == "__main__":
