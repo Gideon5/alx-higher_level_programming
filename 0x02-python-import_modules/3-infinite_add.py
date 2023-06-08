7a7d8a1799a13da10b53dd7ebf76b7f68ebdf877
Gideon Mba <abaaneg@gmail.com>
2023-06-08 12:16:04 -0700

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import sys
 
-sum = 0
 
-for i in range(1, len(sys.argv)):
-    sum += int(sys.argv[i])
-print("{}".format(sum))
+if __name__ == "__main__":
+    import sys
+
+    sum = 0
+    for i in range(1, len(sys.argv)):
+        sum += int(sys.argv[i])
+    print("{}".format(sum))
