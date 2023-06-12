571e88fa7130d292ab2c70fe6e3f74dfe9b87666
Gideon Mba <abaaneg@gmail.com>
2023-06-11 17:34:32 -0700

    task0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,8 @@
 
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
-            print(my_list[i])
+        print("{}".format(my_list[i]))
+
+
+if __name__ == "__main__":
+    print_list_integer(my_list)
