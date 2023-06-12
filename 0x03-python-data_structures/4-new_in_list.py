174540da3584d29b60dbebcc712fe1d956fe89d0
Gideon Mba <abaaneg@gmail.com>
2023-06-11 19:30:25 -0700

    task3

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(list, idx, element):
+    if idx is None:
+        return None
+
+    myList = list[:]
+    if idx >= 0 and idx < len(myList):
+        myList[idx] = element
+    return myList
