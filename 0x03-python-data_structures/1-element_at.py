a6f51c1a2b309a1483c5a9daa5203039bdcc5872
Gideon Mba <abaaneg@gmail.com>
2023-06-11 18:15:29 -0700

    task1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,10 +3,11 @@
 def element_at(my_list, idx):
     if (idx < 0):
         return None
-    elif (idx > len(my_list )):
+    elif (idx > len(my_list)):
         return None
     else:
         return my_list[idx]
+
 
 if __name__ == "__main__":
     element_at()
