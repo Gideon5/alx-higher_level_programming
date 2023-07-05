55a6d92f382f7c3733e03bf442f7a0de765fce2b
Gideon Mba <abaaneg@gmail.com>
2023-07-04 17:47:39 -0700

    al

--- 0-square_matrix_simple.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def square_matrix_simple(matrix=[]):  
-    mult_matrix = [[x*x for x in y] for y in matrix]
-    return mult_matrix
-
-
-if __name__ == "__main__":
-    square_matrix_simple()
